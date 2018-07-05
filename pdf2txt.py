525d4de20d4dfcb009f626353be8caa29b151645
Fabio Ayres <fabioja@insper.edu.br>
2018-07-05 15:53:22 -0300

    Fetching data example from s3.

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,14 +1,32 @@
+import boto3
 import os.path
 import subprocess
 
-def convert_pdf_to_txt(dir_pdf, dir_txt, dir_log, filename_pdf):
+BUCKET_NAME = "justicaestadual"
+S3_FOLDER = "diarios_03_04_2018"
+DIR_PDF = "./data"
+DIR_TXT = "./txt"
+DIR_LOG = "./log"
+
+def download_pdf(filename_pdf, bucket_name=BUCKET_NAME, s3_folder=S3_FOLDER, 
+                 dir_pdf=DIR_PDF):
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(bucket_name)
+
+    key = s3_folder + "/" + filename_pdf
+    bucket.download_file(key, os.path.join(dir_pdf, filename_pdf))
+
+def upload_txt(filename_pdf, bucket_name=BUCKET_NAME, s3_folder=S3_FOLDER, 
+               dir_txt=DIR_TXT):
+    pass
+
+def convert_pdf_to_txt(filename_pdf, dir_pdf=DIR_PDF, dir_txt=DIR_TXT, 
+                       dir_log=DIR_LOG):
     filename_txt = filename_pdf[:-4] + ".txt"
     result = subprocess.run([
         "java", "-jar", "pdfbox-app-2.0.11.jar", "ExtractText", "-debug", 
         os.path.join(dir_pdf, filename_pdf),
-        os.path.join(dir_txt, filename_txt)
-        ], 
-
+        os.path.join(dir_txt, filename_txt)], 
         stderr=subprocess.PIPE)
 
     filename_log = filename_pdf[:-4] + ".log"
@@ -16,8 +34,7 @@ def convert_pdf_to_txt(dir_pdf, dir_txt, dir_log, filename_pdf):
         f.write(result.stderr.decode("utf-8"))
 
 if __name__ == "__main__":
-    dir_pdf = "./data"
-    dir_txt = "./txt"
-    dir_log = "./log"
-    filename_pdf = "03042018_122TRF1.pdf"
-    convert_pdf_to_txt(dir_pdf, dir_txt, dir_log, filename_pdf)
+    filename_pdf = "03042018MA.pdf"
+    download_pdf(filename_pdf)
+    convert_pdf_to_txt(filename_pdf)
+    upload_txt(filename_pdf)
